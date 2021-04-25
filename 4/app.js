6cab0e16c0751deb2f5936e6a4d43603f0223ea3
bordinolafalcon <bordinolafalcon@frba.utn.edu.ar>
2021-04-25 17:20:02 -0300

    4

---
+++ 4/app.js
@@ -0,0 +1,16 @@
+var a = 4;
+
+if (a > 4) {
+    console.log("Variable es mayor a 4");
+} else {
+    console.log("Variable no es mayor a 4");
+}
+/* 
+ * La mayor diferencia es como se imprime en la consola.
+ * cout << "Mensaje" << endl;
+   console.log("Mensaje");
+ * al declarar variables funiona igual que c++ pero en javascript no tienes
+ * que especificar el tipo de dato que va contener la variable.
+ * En C++ existen estos datos: int, float, string, etc.
+   En JS existen estas variables: var, let, const
+ * /
