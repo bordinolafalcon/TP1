43cdd1341161ebe7468fa9db9b4be2b156c8e5df
bordinolafalcon <bordinolafalcon@frba.utn.edu.ar>
2021-04-25 16:46:37 -0300

    3b

---
+++ 3b/3b.cpp
@@ -0,0 +1,20 @@
+// 3b.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float x = 10;
+    float m1 = 20, m2 = 20;
+    float b1 = 30, b2 = 30;
+
+    float y1 = m1 * x + b1;
+    float y2 = m2 * x + b2;
+
+    cout << "Resultado de y1: " << y1 << endl;
+    cout << "Resultado de y2: " << y2 << endl;
+}
+
+
