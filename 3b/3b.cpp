7be73d42ad80d51cfa4cbdf3e43b51e3fc991909
bordinolafalcon <bordinolafalcon@frba.utn.edu.ar>
2021-06-14 09:08:43 -0300

    3b corregido

--- 3b/3b.cpp
+++ 3b/3b.cpp
@@ -2,19 +2,33 @@
 //
 
 #include <iostream>
+
 using namespace std;
 
-int main()
-{
-    float x = 10;
-    float m1 = 20, m2 = 20;
-    float b1 = 30, b2 = 30;
+int main() {
+    float  m1, m2, b1, b2;
+    cout << "Ingrese pendiente 1: " << endl;
+    cin >> m1;
+    cout << "Ingrese pendiente 2: " << endl;
+    cin >> m2;
+    cout << "Ingrese ordenada 1: " << endl;
+    cin >> b1;
+    cout << "Ingrese ordenada 2: " << endl;
+    cin >> b2;
 
-    float y1 = m1 * x + b1;
-    float y2 = m2 * x + b2;
-
-    cout << "Resultado de y1: " << y1 << endl;
-    cout << "Resultado de y2: " << y2 << endl;
+    if (m1 == m2 && b1 == b2) {
+        cout << "Las rectas son iguales, por lo tanto la intersección es la recta." << endl;
+    }
+    else {
+        if (m1 == m2 && b1 != b2) {
+            cout << "Son paralelas, por lo tanto no hay intersección." << endl;
+        }
+        else {
+            float px = (b1 - b2) / (m2 - m1);
+            float py = (m1 * px) + b1;
+            cout << "Son perpendiculares y el punto de intersección es: (" << px << ", " << py << ")" << endl;
+        }
+    }
 }
 
 
